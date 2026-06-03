da1523a030d6a8c074a897d6ef199af0dc5d68b8
HamidGuluzadeh <hamidguluzadeh@gmail.com>
2026-06-03 20:35:58 +0400

    Adding alert for sign in button as functionality

--- script.js
+++ script.js
@@ -109,65 +109,75 @@ const confirmButton = document.querySelector("#confirm-btn");
 const successMessage = document.querySelector(".success-msg");
 const errorMessages = document.querySelectorAll(".error-msg");
 
-confirmButton.addEventListener("click", (event) => {
-    event.preventDefault();
+if (confirmButton) {
+    confirmButton.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    successMessage.innerText = "";
+        successMessage.innerText = "";
 
-    errorMessages.forEach((message) => {
-        message.innerText = "";
+        errorMessages.forEach((message) => {
+            message.innerText = "";
+        });
+
+        const regex1 = /[^a-zA-ZÇçƏəĞğIıİiÖöŞşÜü\s]/;
+        const regex2 = /^\s*$/;
+        const phoneRegex = /^(\+994|0)[\s\-]*([1-9]\d)[\s\-]*\d{3}[\s\-]*\d{2}[\s\-]*\d{2}$/;
+        let isValid = true;
+
+        if (regex2.test(firstName.value)) {
+            firstNameError.innerText = "First name field cannot be empty!";
+            isValid = false;
+        } else if (regex1.test(firstName.value)) {
+            firstNameError.innerText = "First name must contain only letters!";
+            isValid = false;
+        }
+
+        if (regex2.test(lastName.value)) {
+            lastNameError.innerText = "Last name field cannot be empty!";
+            isValid = false;
+        } else if (regex1.test(lastName.value)) {
+            lastNameError.innerText = "Last name must contain only letters!";
+            isValid = false;
+        }
+
+        if (regex2.test(phone.value)) {
+            phoneError.innerText = "Phone number field cannot be empty!";
+            isValid = false;
+        } else if (!phoneRegex.test(phone.value)) {
+            phoneError.innerText = "Incorrect phone number format!";
+            isValid = false;
+        }
+
+        if (regex2.test(reserveDate.value)) {
+            reserveDateError.innerText = "Reservation date is not selected!";
+            isValid = false;
+        }
+
+        if (regex2.test(reserveTime.value)) {
+            reserveTimeError.innerText = "Reservation time is not selected!";
+            isValid = false;
+        }
+        
+        if (table.value == "select") {
+            tableError.innerText = "Table number is not selected!";
+            isValid = false;
+        }
+
+        if (guest.value == "select") {
+            guestError.innerText = "Guest count is not selected!";
+            isValid = false;
+        }
+
+        if (isValid) {
+            successMessage.innerText = "Successfully reserved!";
+        }
     });
+}
 
-    const regex1 = /[^a-zA-ZÇçƏəĞğIıİiÖöŞşÜü\s]/;
-    const regex2 = /^\s*$/;
-    const phoneRegex = /^(\+994|0)[\s\-]*([1-9]\d)[\s\-]*\d{3}[\s\-]*\d{2}[\s\-]*\d{2}$/;
-    let isValid = true;
+const loginButton = document.querySelector(".login-btn");
 
-    if (regex2.test(firstName.value)) {
-        firstNameError.innerText = "First name field cannot be empty!";
-        isValid = false;
-    } else if (regex1.test(firstName.value)) {
-        firstNameError.innerText = "First name must contain only letters!";
-        isValid = false;
-    }
-
-    if (regex2.test(lastName.value)) {
-        lastNameError.innerText = "Last name field cannot be empty!";
-        isValid = false;
-    } else if (regex1.test(lastName.value)) {
-        lastNameError.innerText = "Last name must contain only letters!";
-        isValid = false;
-    }
-
-    if (regex2.test(phone.value)) {
-        phoneError.innerText = "Phone number field cannot be empty!";
-        isValid = false;
-    } else if (!phoneRegex.test(phone.value)) {
-        phoneError.innerText = "Incorrect phone number format!";
-        isValid = false;
-    }
-
-    if (regex2.test(reserveDate.value)) {
-        reserveDateError.innerText = "Reservation date is not selected!";
-        isValid = false;
-    }
-
-    if (regex2.test(reserveTime.value)) {
-        reserveTimeError.innerText = "Reservation time is not selected!";
-        isValid = false;
-    }
-    
-    if (table.value == "select") {
-        tableError.innerText = "Table number is not selected!";
-        isValid = false;
-    }
-
-    if (guest.value == "select") {
-        guestError.innerText = "Guest count is not selected!";
-        isValid = false;
-    }
-
-    if (isValid) {
-        successMessage.innerText = "Successfully reserved!";
-    }
-});
+if (loginButton) {
+    loginButton.addEventListener("click", () => {
+        alert("Authentication will be activated after the development!");
+    });
+}
